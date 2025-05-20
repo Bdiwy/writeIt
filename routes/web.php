f71d14e0303940c7966b0db48740d2ddff4ec10e
Ahmed bdiwy <ahmedbedawi22@gmail.com>
2025-05-20 19:38:56 +0300

    refactor: Simplify route definitions for authentication and improve readability

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Auth;
 
-
 Route::get('/', function () {
     return Auth::check()
         ? view('feed')    
         : view('home');  
-})->name('/');
+})->name('home');
 
-Route::middleware('guest')->group(function () {
-    Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
-    Route::get('/login', [AuthController::class, 'showLogin'])->name('show.login');
-    Route::post('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::controller(AuthController::class)->middleware('guest')->group(function () {
+    Route::get('/register', 'showRegister')->name('show.register');
+    Route::get('/login', 'showLogin')->name('show.login');
+    Route::post('/register', 'register')->name('register');
+    Route::post('/login', 'login')->name('login');
 });
 
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
