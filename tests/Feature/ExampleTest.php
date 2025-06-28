0438938cf4034dfc3611858ec0fcbd386d2b881e
Ahmed bdiwy <ahmedbedawi22@gmail.com>
2025-06-28 18:00:04 +0300

    feat: Add CI workflow for Laravel with MySQL service and PHPUnit testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,55 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        echo "✅ Database migrated\n";
+    }
+    public function test_database_is_configured_correctly()
+    {
+        $this->assertTrue(Schema::hasTable('migrations')); // يتأكد إن القاعدة موجودة
+    }
+    
+
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
     }
+    public function test_user_settings_table_has_expected_columns()
+    {
+        $this->assertTrue(Schema::hasTable('user_settings'), 'Table user_settings does not exist.');
+
+        $columns = Schema::getColumnListing('user_settings');
+
+        $expected = [
+            'id',
+            'user_id',
+            'avatar',
+            'status',
+            'interests',
+            'gender',
+            'bio',
+            'created_at',
+            'updated_at',
+        ];
+
+        foreach ($expected as $column) {
+            $this->assertContains($column, $columns, "Missing column: $column");
+        }
+    }
+
 }
